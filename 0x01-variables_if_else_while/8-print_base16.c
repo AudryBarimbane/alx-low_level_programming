da1076d5ae6ab559c41814fcb36923abafa61d2a
Audry <drybary@gmail.com>
2023-03-20 16:41:19 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ ** main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned char c = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	c = '1';
+	for (i = 0; i < 6; i++)
+	{
+		putchar('0' + c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
