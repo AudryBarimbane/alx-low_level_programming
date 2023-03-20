54fe47e45870976859ee1fdb21ce8e33331b3be8
Audry <drybary@gmail.com>
2023-03-20 05:01:16 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char c;
 
-	for (c = "a"; c <= "z"; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c == "q" || c == "e")
+		if (c == 'q' || c == 'e')
 			continue;
 		putchar(c);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
