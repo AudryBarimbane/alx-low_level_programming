3c538b4e23fec8ac978f04468acd7b9ab8adb9f0
Audry <drybary@gmail.com>
2023-03-30 04:34:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,14 +16,14 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] = 32;
+			s[i] -= 32;
 
 		for (j = 0; j < 13; j++)
 		{
 			if (s[i] == sp[j])
 			{
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 32] -= 32;
+					s[i + 1] -= 32;
 			}
 		}
 	}
