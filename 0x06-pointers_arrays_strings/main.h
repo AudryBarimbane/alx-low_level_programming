3ff320089d22c2208c20ec1675bb9832274e0519
Audry <drybary@gmail.com>
2023-03-29 23:38:11 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,3 +11,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
+
+#endif
