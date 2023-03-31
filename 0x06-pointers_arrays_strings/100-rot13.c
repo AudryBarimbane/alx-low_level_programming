32653711322f75b700e30eef1004a99ef34b3721
Audry <drybary@gmail.com>
2023-03-31 04:23:44 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: string to be encoded
+ *
+ * Return: a string rotated
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+	char s1[] = {ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz};
+	char s2[] = {NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
