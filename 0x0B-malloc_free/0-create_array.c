795fde3f84bc585c1890b02d343a9a6483cb3fb0
Audry <drybary@gmail.com>
2023-04-11 05:34:03 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,16 +18,16 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = (char *)malloc(sizeof(char) * size);
+	p = malloc(sizeof(c) * size);
 
 	if (p == NULL)
 		return (0);
 
 	for (i = 0; i < size; i++)
-	{
+
 		*(p + i) = c;
-	}
-	*(p + i) = '\0';
+
+
 
 	return (p);
 }
