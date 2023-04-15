1ea3540fced472c105eb413608d4c2ddb9d1ff55
Audry <drybary@gmail.com>
2023-04-14 18:37:03 -0700

    1-strdup.c

---
+++ 0x0C-more_malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc.
+ * @nmemb: numb elements
+ * @size: size of bytes
+ *
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size; i++)
+			s[i] = 0;
+
+	return (s);
+}
