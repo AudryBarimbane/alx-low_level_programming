a679e5c12b8360234334444be848e7615391621c
Audry <drybary@gmail.com>
2023-04-15 18:52:25 -0700

    argc,argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, n,  = 0;
+	char *nd;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strtol(argv[i], &flag, 10);
+		if (*nd)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			s += n;
+		}
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
