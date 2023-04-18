952a394eaac165f6f5cd0c739cea66beef727079
Audry <drybary@gmail.com>
2023-04-18 12:35:18 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
