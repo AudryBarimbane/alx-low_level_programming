e85f619e4d691db83b27983ca5b9d068eaed56dd
Audry <drybary@gmail.com>
2023-04-19 00:09:29 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,15 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int l;
 
-	l = 0;
-
-	if (*s == 0)
-		return (l);
+	if (*s == '\0')
+		return (NULL);
 	else
-	{
-		l++;
-		_strlen_recursion(s + 1);
-	}
+		return (1 + _strnlen_recursion(s + 1));
 }
