614cabf3d1dc0a99ed1e1559ac5a5a240d7cccac
Audry Barimbane <drybary@gmail.com>
2023-06-14 10:51:03 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,8 @@
 
 int _atoi(char *s)
 {
-	int i = 0, j = 1, k = 0;
+	int i = 0, j = 1, 
+	unsigned int k = 0;
 
 	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
 	{
@@ -16,7 +17,7 @@ int _atoi(char *s)
 			j *= -1;
 		i++;
 	}
-	while (s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'))
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
 	{
 		k = (k * 10) + (s[i] - '0');
 		i++;
