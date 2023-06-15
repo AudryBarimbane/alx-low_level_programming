3b7c5c25191b2d444012ee8b3349228fd52d0bac
Audry Barimbane <drybary@gmail.com>
2023-06-14 18:09:07 -0700

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number
+ * @argv: array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	print("%d\n", argc - 1);
+	return (0);
+}
