13136d4a15300ab7c581a871946c16110d7860b2
Audry Barimbane <drybary@gmail.com>
2023-06-21 18:40:49 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,34 +8,36 @@
  *
  * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i, b;
-	char *Tab;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
-
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	b = atoi(argv[1]);
-	if (b < 0)
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	Tab = (char *)main;
-	for (i = 0; i < b; i++)
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == b - 1)
+		if (i == bytes - 1)
 		{
-			printf("%02hhx\n", Tab[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx\n", Tab[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
-
-
