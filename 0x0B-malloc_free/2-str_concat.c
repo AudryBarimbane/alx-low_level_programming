a0c5606d68ec9148f6605abbf3ca7bc99238a7a5
Audry Barimbane <drybary@gmail.com>
2023-11-20 00:56:16 -0800

    Task 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: string
+ * @s2: string
+ *
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	s = malloc(sizeof(char) * (i + j + 1));
+
+	if (s == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		s[i] = s2[j];
+		i++, j++;
+	}
+	s[i] = '\0';
+	return (s);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to
+ * a 2 dimensional array of integers.
+ * @width: width
+ * @height: height
+ *
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **m;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	m = malloc(sizeof(int *) * height);
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		m[i] = malloc(sizeof(int) * width);
+		if (m[i] == NULL)
+		{
+			for (; i <= 0; i--)
+				free(m[i]);
+			free(m);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			m[i][j] = 0;
+	}
+	return (m);
+}
